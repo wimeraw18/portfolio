b4a5c2bc59ef061e5863855c02ffd6212adebf13
Alaric Wimer <wimeraw18@gmail.com>
2018-10-04 17:12:21 -0700

    toggle bars and close icons when clicked on mobile screens

--- scripts.js
+++ scripts.js
@@ -2,9 +2,14 @@ const menuButton = document.querySelector("#nav-button");
 const navbar = document.querySelector("#navbar");
 const email = document.querySelector("#email");
 const phone = document.querySelector("#phone");
+const icon = document.querySelector("button");
+const close = document.querySelector(".fa-times");
+const bars = document.querySelector(".fa-bars");
 
 menuButton.addEventListener("click", function() {
   navbar.classList.toggle("hide");
+  bars.classList.toggle("hide");
+  close.classList.toggle("hide");
 });
 
 email.innerHTML = "<h3>wimeraw18@gmail.com</h3>";
