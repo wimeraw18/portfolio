bc2f1fdcc5f0b6422941af6af74827a753566d80
Alaric Wimer <wimeraw18@gmail.com>
2018-10-05 14:09:23 -0700

    fix chrome viewport height bug

--- mobile-chrome-vh-fix.js
+++ mobile-chrome-vh-fix.js
@@ -1,21 +1,3 @@
-const menuButton = document.querySelector("#nav-button");
-const navbar = document.querySelector("#navbar");
-const email = document.querySelector("#email");
-const phone = document.querySelector("#phone");
-const icon = document.querySelector("button");
-const close = document.querySelector(".fa-times");
-const bars = document.querySelector(".fa-bars");
-
-menuButton.addEventListener("click", function() {
-  navbar.classList.toggle("hide");
-  bars.classList.toggle("hide");
-  close.classList.toggle("hide");
-});
-
-email.innerHTML = "<h3>wimeraw18@gmail.com</h3>";
-phone.innerHTML = "<h3>608-960-2211</h3>";
-
-/* FIX CHROME VIEWPORT HEIGHT BUG ON MOBILE SCREENS */
 var VHChromeFix = function(selectors) {
   var self = this;
   var userAgent = navigator.userAgent.toLowerCase();
